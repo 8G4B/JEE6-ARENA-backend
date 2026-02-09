9f7e3042cf95fea67701541aa874dd48872a7416
재욱 <haensol976520@gmail.com>
2026-02-09 16:58:00 +0900

    chore: setup MariaDB infrastructure and database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PointsModule } from './points/points.module';
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'hammer',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    PointsModule,
+    GamesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
